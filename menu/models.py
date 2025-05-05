31cd5466890069059cb6b19c016f541ef88c8d50
karranaveen045 <venkykarra054@gmail.com>
2025-05-05 14:00:15 +0530

    food crud operations

--- menu/models.py
+++ menu/models.py
@@ -25,7 +25,7 @@ class FoodItem(models.Model):
     description=models.TextField(max_length=250,blank=True)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     image=models.ImageField(upload_to='foodimages')
-    is_available=models.BooleanField(default=True)
+    is_available=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- vendor/views.py
+++ vendor/views.py
@@ -11,7 +11,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required,user_passes_test
 from accounts.views import check_role_vendor
 from menu.models import Category,FoodItem
-from menu.forms import CategoryForm
+from menu.forms import CategoryForm,FoodItemForm
 
 def get_vendor(request):
     vendor= Vendor.objects.get(user=request.user)
@@ -67,6 +67,8 @@ def fooditems_by_category(request,pk=None):
         'category':category
     }
     return render(request, 'vendor/fooditems_by_category.html',context)
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def add_category(request):
     if request.method=='POST':
         form=CategoryForm(request.POST)
@@ -86,6 +88,8 @@ def add_category(request):
         'form':form,
     }
     return render(request, 'vendor/add_category.html',context)
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 
 def edit_category(request,pk=None):
     category=get_object_or_404(Category,pk=pk)
@@ -108,6 +112,8 @@ def edit_category(request,pk=None):
         'category':category,
     }
     return render(request, 'vendor/edit_category.html',context)
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 
 def delete_category(request,pk=None):
     category=get_object_or_404(Category,pk=pk)
@@ -115,4 +121,54 @@ def delete_category(request,pk=None):
     messages.success(request,'Category has been deleted successfully!')
     return redirect('menu_builder')
 
+def add_food(request):
+    if request.method=='POST':
+        form=FoodItemForm(request.POST,request.FILES)
+        if form.is_valid():
+            foodtitle=form.cleaned_data['food_title']
+            food=form.save(commit=False)
+            food.vendor=get_vendor(request)
+            food.slug=slugify(foodtitle)
+            form.save()
+            messages.success(request,'Category added successfully!')
+            return redirect('fooditems_by_category',food.category.id)
+        else:
+            print(form.errors)
+    else:
+        form=FoodItemForm()
+        #modify this form
+        form.fields['category'].queryset=Category.objects.filter(vendor=get_vendor(request))
+    context={
+        'form':form,
+    }
+    return render(request, 'vendor/add_food.html',context)
+def edit_food(request,pk=None):
+    food=get_object_or_404(FoodItem,pk=pk)
+    if request.method=='POST':
+        form=FoodItemForm(request.POST,request.FILES,instance=food)
+        if form.is_valid():
+            foodtitle=form.cleaned_data['food_title']
+            food=form.save(commit=False)
+            food.vendor=get_vendor(request)
+            food.slug=slugify(foodtitle)
+            form.save()
+            messages.success(request,'Food item updated successfully!')
+            return redirect('fooditems_by_category',food.category.id)
+        else:
+            print(form.errors)
+    else:
+        form=FoodItemForm(instance=food)
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
+    context={
+        'form':form,
+        'food':food,
+    }
+    return render(request, 'vendor/edit_food.html',context)
 
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def delete_food(request, pk=None):
+    food = get_object_or_404(FoodItem, pk=pk)
+    food.delete()
+    messages.success(request, 'Food Item has been deleted successfully!')
+    return redirect('fooditems_by_category', food.category.id)
