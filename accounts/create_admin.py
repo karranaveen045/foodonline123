a5e71557ede12686fc0104b2f59cce225c415fdf
karranaveen045 <venkykarra054@gmail.com>
2025-07-01 12:06:11 +0530

    Update create_admin.py

--- accounts/create_admin.py
+++ accounts/create_admin.py
@@ -1,10 +1,19 @@
-# create_admin.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodonline.settings")
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin02@gmail.com", "admin123")
-    print("✅ Superuser created.")
+    User.objects.create_superuser(
+        username="rohit123",
+        email="rohit123@example.com",
+        password="admin123"
+    )
+    print("Superuser created.")
 else:
-    print("⚠️ Superuser already exists.")
+    print("Superuser already exists.")
